13c001cb4ef22ae27a49ed5b0500affc589adc93
Don <doncarlier@gmail.com>
2018-07-30 10:28:51 +0200

    lecture-53-weather

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-export function selectBook(book) {
-    // SelectBook is an actioncreator it needs to return an action.
-    // An object with a type property
-    return {
-
-        type: 'BOOK_SELECTED',
-        payload: book
-   };
-}
--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return[
-      {title: 'JS', pages: 101},
-      {title: 'Actionscript', pages: 39},
-      {title: 'React', pages: 1},
-      {title: 'HTML', pages: 99}
-  ]
-}
